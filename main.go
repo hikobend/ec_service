3f073e2756d9783c530989b1051b8e830c8c083d
hikobend <hiko.bkend@gmaiul.com>
2022-12-04 18:20:40 +0900

    UPDATE

--- main.go
+++ main.go
@@ -34,12 +34,13 @@ type Product struct { // DB
 func main() {
 	r := gin.Default()
 	r.POST("/create", Insert)
-	r.PATCH("/product/:id/name", NameUpdate) // 名前を更新
-	// r.PATCH("/product/:id/category", CategoryUpdate)       // 名前を更新
-	// r.PATCH("/product/:id/price", PriceUpdate)             // 名前を更新
-	// r.PATCH("/product/:id/stock", StockUpdate)             // 名前を更新
-	// r.PATCH("/product/:id/brand", BrandUpdate)             // 名前を更新
-	// r.PATCH("/product/:id/description", DescriptionUpdate) // 名前を更新
+	r.PATCH("/product/:id", AllUpdate)                     // 全て更新
+	r.PATCH("/product/:id/name", NameUpdate)               // 名前を更新
+	r.PATCH("/product/:id/category", CategoryUpdate)       // カテゴリーを更新
+	r.PATCH("/product/:id/price", PriceUpdate)             // 値段を更新
+	r.PATCH("/product/:id/stock", StockUpdate)             // 在庫を更新
+	r.PATCH("/product/:id/brand", BrandUpdate)             // ブランドを更新
+	r.PATCH("/product/:id/description", DescriptionUpdate) // 説明を更新
 
 	r.Run()
 }
@@ -81,4 +82,136 @@ func NameUpdate(c *gin.Context) {
 		log.Fatal(err)
 	}
 	update.Exec(json.Name, id)
+}
+
+func AllUpdate(c *gin.Context) {
+	db, err := sql.Open("mysql", "root:password@(localhost:3306)/local?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var json Product_JSON
+	c.ShouldBindJSON(&json)
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	update, err := db.Prepare("UPDATE product SET name = ?, category = ?, price = ?, stock = ?, brand = ?, description = ? WHERE id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	update.Exec(json.Name, json.Category, json.Price, json.Stock, json.Brand, json.Description, id)
+}
+
+func CategoryUpdate(c *gin.Context) {
+	db, err := sql.Open("mysql", "root:password@(localhost:3306)/local?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var json Product_JSON
+	c.ShouldBindJSON(&json)
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	update, err := db.Prepare("UPDATE product SET category = ? WHERE id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	update.Exec(json.Category, id)
+}
+
+func PriceUpdate(c *gin.Context) {
+	db, err := sql.Open("mysql", "root:password@(localhost:3306)/local?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var json Product_JSON
+	c.ShouldBindJSON(&json)
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	update, err := db.Prepare("UPDATE product SET price = ? WHERE id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	update.Exec(json.Price, id)
+}
+
+func StockUpdate(c *gin.Context) {
+	db, err := sql.Open("mysql", "root:password@(localhost:3306)/local?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var json Product_JSON
+	c.ShouldBindJSON(&json)
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	update, err := db.Prepare("UPDATE product SET stock = ? WHERE id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	update.Exec(json.Stock, id)
+}
+
+func BrandUpdate(c *gin.Context) {
+	db, err := sql.Open("mysql", "root:password@(localhost:3306)/local?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var json Product_JSON
+	c.ShouldBindJSON(&json)
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	update, err := db.Prepare("UPDATE product SET brand = ? WHERE id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	update.Exec(json.Brand, id)
+}
+
+func DescriptionUpdate(c *gin.Context) {
+	db, err := sql.Open("mysql", "root:password@(localhost:3306)/local?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var json Product_JSON
+	c.ShouldBindJSON(&json)
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	update, err := db.Prepare("UPDATE product SET description = ? WHERE id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	update.Exec(json.Description, id)
 }
